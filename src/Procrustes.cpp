270aaf963d33ec6ebbad79ba90e6816ee353d06b
tqsmith <thomas.smith7@wilkes.edu>
2023-07-27 14:47:14 -0400

    Started Procrustes.

--- src/Procrustes.cpp
+++ src/Procrustes.cpp
@@ -17,9 +17,6 @@
 // Used for creating, destroying, deep copying, and shuffling matrices.
 #include "MatrixOperations.hpp"
 
-// Used for eigenvalue/eigenvector computation when 3 < k < n.
-#include "NumericalRecipesInC.hpp"
-
 // Used for the basic math operatons sqrt, cos, and acos.
 #include <cmath>
 
@@ -33,5 +30,100 @@
 //  int k -> The dimension of each point. Assume k = 1, 2, 3.
 // Returns: double, The Procrustes statistic.
 double procrustes(double** Xc, double** Yc, double** C, double** CT_C, int n, int k) {
-    
+
+    // Next, we calculate the trace of XTc_Xc and YTc_Yc
+    //  We are taking the dot-product of each column
+    //  with itself for each matrix. This corresponds
+    //  to the diagonal elements.
+    double trXTc_Xc = 0;
+    double trY = 0;
+
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) {
+            trXTc_Xc += (Xc[j][i]) * (Xc[j][i]);
+            trYTc_Yc += (Yc[j][i]) * (Yc[j][i]); 
+        }
+    }
+
+    // Calculate C = YTc_Xc and store in C.
+    for (int a = 0; a < k; a++) {
+        for (int b = 0; b < k; b++) {
+            C[a][b] = 0;
+            for (int c = 0; c < n; c++) {
+                C[a][b] += (Yc[c][a]) * (Xc[c][b]);
+            }
+        }
+    }
+
+    // Calculate CT_C and store in CT_C.
+    for (int a = 0; a < k; a++) {
+        for (int b = 0; b < k; b++) {
+            CT_C[a][b] = 0;
+            for (int c = 0; c < k; c++) {
+                CT_C[a][b] += C[c][a] * C[c][b];
+            }
+        }
+    }
+
+    // Calculate the sum of the singular values of C,
+    //  or equivalently, the sum of the eigenvalues
+    //  for CT_C.
+    double trLambda = 0;
+
+    // We solve the characteristic polynomial of the matrix using the quadratic
+    //  and cubic formulas for the roots. Code taken from Numerical Recipes
+    //  in C, Third Edition.
+    switch (k) {
+        case 1:
+            trLambda = sqrt(CT_C[0][0]);
+            break;
+
+        case 2:
+            // Create the coefficients of the quadratic and the determinant.
+            // Our quadratic is in the form of x^2+bx+c.
+            double b = -(temp2[0][0] + temp2[1][1]);
+            double c = (temp2[0][0] * temp2[1][1]) - (temp2[0][1] * temp2[1][0]);
+            double det = sqrt(b * b - 4 * c);
+
+            // Calculate q.
+            double q = b < 0 ? -0.5 * (b - det) : -0.5 * (b + det);
+
+            // Calculate trace.
+            trLambda += sqrt(q) + sqrt(c / q);
+            break;
+
+        case 3:
+            // Calculate our coefficients.
+            double a = -(temp2[0][0] + temp2[1][1] + temp2[2][2]);
+
+            // Sum of minors along the diagonal.
+            double b = ((temp2[1][1] * temp2[2][2]) - (temp2[1][2] * temp2[2][1]))
+                        + ((temp2[0][0] * temp2[2][2]) - (temp2[0][2] * temp2[2][0]))
+                        + ((temp2[0][0] * temp2[1][1]) - (temp2[0][1] * temp2[1][0]));
+
+            // -detA
+            double c = -(temp2[0][0] * ((temp2[1][1] * temp2[2][2]) - (temp2[2][1] * temp2[1][2])) 
+                        - temp2[0][1] * ((temp2[1][0] * temp2[2][2]) - (temp2[1][2] * temp2[2][0])) 
+                        + temp2[0][2] * ((temp2[1][0] * temp2[2][1]) - (temp2[1][1] * temp2[2][0])));
+
+            // Now, we solve the cubic.
+            double Q = (a * a - 3 * b) / 9;
+            double R = (2 * a * a * a - 9 * a * b + 27 * c) / 54;
+            double theta = acos(R / sqrt(Q * Q * Q));
+
+            // Now calculate the sume of the roots.
+            trLambda += sqrt(-2 * sqrt(Q) * cos(theta / 3) - (a/3));
+            trLambda += sqrt(-2 * sqrt(Q) * cos((theta + 2 * M_PI) / 3) - (a/3));
+            trLambda += sqrt(-2 * sqrt(Q) * cos((theta - 2 * M_PI) / 3) - (a/3));
+            break;
+
+        default:
+            // Note: Our default case would use the eigenvalue computations from 
+            //          NumericalRecipesInC.hpp.
+            break;
+    }
+
+    // Return the Procrustes statisitc.
+    return (trLambda * trLambda) / (trXTc_Xc * trYTc_Yc);
+
 }
