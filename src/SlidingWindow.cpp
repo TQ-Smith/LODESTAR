fc78f48af0964367dd058467915e3d2d18c80edb
TQ-Smith <thomasqsmith2000@gmail.com>
2023-10-18 14:21:27 -0400

    Working on Sliding Window

--- src/SlidingWindow.cpp
+++ src/SlidingWindow.cpp
@@ -8,6 +8,8 @@
 
 #include "SlidingWindow.hpp"
 
+#include <iostream>
+
 // Allocates a window with the specified attributes.
 // Accepts:
 //  string chromosome -> The chromosome the window is on.
@@ -42,6 +44,41 @@ list<window*>* sliding_window(VCFParser* parser, int hap_size, int window_hap_si
     bool isMonomorphic;
     bool isComplete;
     Genotype* genotypes = new Genotype[n];
+
+    // Variables needed to keep track of window information.
+    string previous_chromosome = "";
+    int previous_position;
+    int start_position;
+    int num_current_loci = 0;
+
+    // Read in the loci from the VCF.
+    while(parser -> getNextLocus(&chromosome, &position, &isMonomorphic, &isComplete, genotypes)) {
+        
+        // If the site is monomorphic or incomplete, then skip over the site.
+        if (isMonomorphic || !isComplete) {
+            continue;
+        }
+
+        // If new chromosome or finished window, then preform MDS, save window, and reset values for new window.
+        if (chromosome != previous_chromosome || (hap_size * window_hap_size) == num_current_loci) {
+
+            // We have to create the first window.
+            if (num_current_loci == 0) {
+            } else {
+                windows -> push_back(create_window(previous_chromosome, start_position, previous_position, num_current_loci, NULL));
+                num_current_loci = 0;
+            }
+
+            start_position = position;
+
+        }
+
+        num_current_loci++;
+        previous_chromosome = chromosome;
+        previous_position = position;
+
+    }
+    windows -> push_back(create_window(previous_chromosome, start_position, previous_position, num_current_loci, NULL));
 
 
     // Free allocated genotypes array.
--- unit_tests/unit_test_sliding_window.cpp
+++ unit_tests/unit_test_sliding_window.cpp
@@ -23,9 +23,22 @@ using namespace std;
 int main() {
 
     // Open our VCF file.
-
+    cout << endl;
     cout << "Opening sample2.vcf ..." << endl;
     VCFParser* parser = new VCFParser("unit_tests/sample2.vcf");
     cout << endl;
+
+    // Print the number of samples.
+    int n = parser -> getNumberOfSamples();
+    cout << "The VCF file has " << n << " samples." << endl;
+    cout << endl;
+
+    cout << "We create a sliding window with haplotype size of 1 SNPs, a window size of 2 haplotypes, and an offset of 1 haplotypes." << endl;
+    cout << "We find the windows to be:" << endl;
+    list<window*>* windows = sliding_window(parser, 1, 2, 1, n);
+    for (list<window*>::iterator it = windows -> begin(); it != windows -> end(); it++){
+        cout << (*it) -> chromosome << ": " << (*it) -> start_position << " to " << (*it) -> end_position << " with " << (*it) -> num_loci << " SNPs." << endl;
+        destroy_window(&*it);
+    }
 
 }
