5bc47f6722c6d7d7a9235242c9020413552a5162
TQ-Smith <thomasqsmith2000@gmail.com>
2024-02-15 11:26:46 -0500

    Finished Encoder with Godel Encoding ...

--- src/test.c
+++ src/test.c
@@ -17,85 +17,13 @@ void print_window_info(Window* window) {
     printf("\n");
 }
 
-void print_encoder_info(HaplotypeEncoder* encoder) {
-    printf("Chromosome: %s\n", ks_str(encoder -> chromosome));
-    printf("Start locus: %d\n", encoder -> startLocus);
-    printf("End locus: %d\n", encoder -> endLocus);
-    printf("Number of loci: %d\n", encoder -> numLoci);
-    printf("Sample Haplotypes:\n");
-    for (int i = 0; i < encoder -> numSamples; i++)
-        printf("Sample %d -> %lf/%lf\n", i + 1, encoder -> leftHaplotype[i], encoder -> rightHaplotype[i]);
-}
-
 int main() {
 
     VCFGenotypeParser* parser = init_vcf_genotype_parser("./data/haplotype_tree_test.vcf.gz");
     HaplotypeEncoder* encoder = init_haplotype_encoder(parser -> num_samples);
     
-    printf("\nTest 1\n");
-    printf("---------\n");
-    printf("Read in whole chromosomes:\n\n");
-    get_next_haplotype(parser, encoder, false, 10);
-    printf("First Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 10);
-    printf("\nSecond Haplotype:\n");
-    print_encoder_info(encoder);
-    destroy_vcf_genotype_parser(parser);
-
-    printf("\n");
-    parser = init_vcf_genotype_parser("./data/haplotype_tree_test.vcf.gz");
-    printf("\nTest 2\n");
-    printf("---------\n");
-    printf("Read in 2-loci haplotypes:\n\n");
-    get_next_haplotype(parser, encoder, false, 2);
-    printf("First Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 2);
-    printf("\nSecond Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 2);
-    printf("\nThird Haplotype:\n");
-    print_encoder_info(encoder);
-    destroy_vcf_genotype_parser(parser);
-
-    printf("\n");
-    parser = init_vcf_genotype_parser("./data/haplotype_tree_test.vcf.gz");
-    printf("\nTest 3\n");
-    printf("---------\n");
-    printf("Read in 1-locus haplotypes:\n\n");
-    get_next_haplotype(parser, encoder, false, 1);
-    printf("First Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 1);
-    printf("\nSecond Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 1);
-    printf("\nThird Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 1);
-    printf("\nFourth Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, false, 1);
-    printf("\nFifth Haplotype:\n");
-    print_encoder_info(encoder);
-    destroy_vcf_genotype_parser(parser);
-
-    printf("\n");
-    parser = init_vcf_genotype_parser("./data/haplotype_tree_test.vcf.gz");
-    printf("\nTest 4\n");
-    printf("---------\n");
-    printf("Read in whole chromosomes and collapse genotypes:\n\n");
-    get_next_haplotype(parser, encoder, true, 10);
-    printf("First Haplotype:\n");
-    print_encoder_info(encoder);
-    get_next_haplotype(parser, encoder, true, 10);
-    printf("\nSecond Haplotype:\n");
-    print_encoder_info(encoder);
-    destroy_vcf_genotype_parser(parser);
-
-    printf("\n");
     
+    destroy_vcf_genotype_parser(parser);
     destroy_haplotype_encoder(encoder);
 
     return 0;
