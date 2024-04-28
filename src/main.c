b59d58431d38766da1697cf4e48b3271798e38cc
TQ-Smith <thomasqsmith2000@gmail.com>
2024-04-28 14:18:30 -0400

    Started CLI ...

--- src/main.c
+++ src/main.c
@@ -7,9 +7,30 @@
 
 #include "ProcrustesAnalysis.h"
 
-#include <stdio.h>
+#include "../lib/ketopt.h"
 
-#include <stdlib.h>
+
+void print_help() {
+    fprintf(stderr, "LODESTAR v1.0 May 2024\n");
+    fprintf(stderr, "----------------------\n\n");
+    fprintf(stderr, "Written by T. Quinn Smith\n");
+    fprintf(stderr, "Principal Investigator: Zachary A. Szpiech\n");
+    fprintf(stderr, "The Pennsylvania State University\n\n");
+    fprintf(stderr, "Usage: lodestar [options] -i <input.vcf.gz> -o <output_basename>\n");
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "   --help              Prints help menu and exits.\n");
+    fprintf(stderr, "   --version           Prints version number and exits.\n");
+    fprintf(stderr, "   -i STR              Path to input vcf file.\n");
+    fprintf(stderr, "   -o STR              Output base names for files.\n");
+    fprintf(stderr, "   -h INT              Number of loci in a haplotype. Default 10.\n");
+    fprintf(stderr, "   -w INT              Number of haplotypes in a window. Default 100.\n");
+    fprintf(stderr, "   -s INT              Number of haplotypes to increment the sliding window. Default 10.\n");
+    fprintf(stderr, "   -k INT              Dimension to project samples into. Must be less than number of samples. Default 2.\n");
+    fprintf(stderr, "   --threads INT       Number of threads to use in computation.\n");
+    fprintf(stderr, "   --similarity        Compute similarity between sets of points instead of dissimilarity.\n");
+    fprintf(stderr, "   --global            Compute only the global set of points.\n");
+
+}
 
 void print_window_info(Window* window, int n, int k) {
     printf("Window Number: %d\n", window -> winNum);
@@ -27,8 +48,53 @@ void print_window_info(Window* window, int n, int k) {
     printf("\n\n");
 }
 
-int main () {
+static ko_longopt_t long_options[] = {
+    {"help",            ko_no_argument,       300},
+    {"version",         ko_no_argument,       301},
+    {"similarity",      ko_no_argument,       302},
+    {"global",          ko_no_argument,       303},
+    {"threads",         ko_optional_argument, 304},
+    {"perms",           ko_optional_argument, 305},
+    {0, 0, 0}
+};
 
+int main (int argc, char *argv[]) {
+
+    bool help = false;
+    char* input_file = NULL;
+    char* output_basename = NULL;
+    int HAP_SIZE = 10;
+    int WINDOW_SIZE = 100;
+    int STEP_SIZE = 10;
+    int k = 2;
+    bool similarity = false;
+    bool global = false;
+    int threads = 1;
+
+    const char *opt_str = "i:o:h:w:s:k:v";
+    ketopt_t options = KETOPT_INIT;
+    int c;
+
+    while ((c = ketopt(&options, argc, argv, 1, opt_str, long_options)) >= 0) {
+        switch (c) {
+            case ':':
+                fprintf(stderr, "Error! Option is missing an argument! Exiting ...\n");
+                return 1;
+            case '?':
+                fprintf(stderr, "Error! \"%s\" is unknown! Exiting ...\n", argv[options.i - 1]);
+                return 1;
+            case 300:
+                print_help();
+                return 0;
+            case 'v':
+            case 301:
+                fprintf(stderr, "Version 1.0 May 2024.\n");
+                return 0;
+        }
+	}
+	options = KETOPT_INIT;
+
+    /*
     int k = 1;
     int NUM_THREADS = 1;
     int HAP_SIZE = 1, STEP_SIZE = 3, WINDOW_SIZE = 4;
@@ -47,6 +113,7 @@ int main () {
     free(windows);
     destroy_vcf_locus_parser(parser);
     destroy_haplotype_encoder(encoder);
+    */
 
     return 0;
 
