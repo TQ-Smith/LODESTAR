44e018dd6543d96a88f664df6434a125ea29b76e
TQ-Smith <thomasqsmith2000@gmail.com>
2024-05-05 16:54:50 -0400

    Documenting Procrustes ...

--- src/ProcrustesAnalysis.c
+++ src/ProcrustesAnalysis.c
@@ -1,14 +1,27 @@
 
-#include "ProcrustesAnalysis.h"
+// File: ProcrustesAnalysis.c
+// Date: 5 May 2024
+// Author: T. Quinn Smith
+// Principal Investigator: Dr. Zachary A. Szpiech
+// Purpose: Compute Procrustes statistic between two sets of points and perform permutation test.
+// References: Wang et al, Procrustes Analysis in Population Genetics, 2010.
 
+#include "ProcrustesAnalysis.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-#include <stdio.h>
-
-#include <stdlib.h>
-
+// Fortran matrices are stored as vectors in column-major form.
+//  Covariance matrices are symmetric so this is not explicitly necessary,
+//  but it is good to keep things consistent.
 #define COL_MAJOR(i, j, K) (j * K + i)
 
+// Fischer-Yates shuffle algorithm to shuffle the rows of a matrix.
+//  NOTE: Random number generator needs to be seeded before calling function.
+// Accepts:
+//  double** matrix -> The matrix to be shuffled.
+//  int m -> The number of rows in the matrix.
+// Returns: void.
 void shuffle_real_matrix(double** matrix, int m) {
     double* temp = NULL;
     int j;
@@ -20,28 +33,32 @@ void shuffle_real_matrix(double** matrix, int m) {
     }
 }
 
-double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, RealSymEigen* eigen, int N, int K, bool transform, bool similarity) {
+double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, RealSymEigen_t* eigen, int N, int K, bool transform, bool similarity) {
 
-    double trX = 0, trY = 0, trLambda = 0;
+    // Trace of (Xc^T)Xc, trace of (Yc^T)Yc.
+    double trX = 0, trY = 0;
 
-    for (int i = 0; i < K; i++) {
-        for (int j = 0; j < N; j++) {
-            trX += Xc[j][i] * Xc[j][i];
-            trY += Yc[j][i] * Yc[j][i]; 
-        }
-    }
-
+    // For convenience, we rename Z and A to C and covC, respecively.
+    //  We will perfrom SVD on C, which means we will take the eigen-decomposition
+    //  of the covariance of C.
     double* C = eigen -> Z;
     double* covC = eigen -> A;
 
+    // Calculate C = (Yc^T)Xc. Simultaneously calculate the trace of (Xc^T)Xc and trace of (Yc^T)Yc.
     for (int i = 0; i < K; i++) {
         for (int j = 0; j < K; j++) {
             C[COL_MAJOR(i, j, K)] = 0;
-            for (int k = 0; k < N; k++)
+            for (int k = 0; k < N; k++) {
                 C[COL_MAJOR(i, j, K)] += Yc[k][i] * Xc[k][j];
+                if (j == 0) {
+                    trX += Xc[k][i] * Xc[k][i];
+                    trY += Yc[k][i] * Yc[k][i]; 
+                }
+            }
         }
     }
 
+    // Calculate covC = (C^T)C.
     for (int i = 0; i < K; i++) {
         for (int j = 0; j < K; j++) {
             covC[COL_MAJOR(i, j, K)] = 0;
@@ -49,7 +66,13 @@ double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, Re
                 covC[COL_MAJOR(i, j, K)] += C[COL_MAJOR(k, i, K)] * C[COL_MAJOR(k, j, K)];
         }
     }
-    
+
+    // Trace of the lambda matrix.
+    double trLambda = 0;
+
+    // In the case we do not transform the X coordinates, we just need the trace of the lambda matrix.
+    //  When K = 1, 2, 3, we can calculate the eigenvalues of covC directly as the roots of its characteric equation.
+    //  We used Numerical Recipes in C, Third Edition to solve the quadratic and cubic equations.
     if (!transform) {
         double a0, a1, a2, b, c, det, q, r, theta, root1, root2, root3;
         switch (K) {
@@ -59,13 +82,12 @@ double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, Re
             case 2:
                 b = -(covC[COL_MAJOR(0, 0, K)] + covC[COL_MAJOR(1, 1, K)]);
                 c = (covC[COL_MAJOR(0, 0, K)] * covC[COL_MAJOR(1, 1, K)]) - (covC[COL_MAJOR(0, 1, K)] * covC[COL_MAJOR(1, 0, K)]);
-                
                 det = sqrt(b * b - 4 * c);
                 q = b < 0 ? -0.5 * (b - det) : -0.5 * (b + det);
-                
                 trLambda += sqrt(q) + sqrt(c / q);
                 break;
             case 3:
+                // Cubic is in the form x^3 + a0x^2 + a1x^1 + a0 = 0.
                 a2 = -(covC[COL_MAJOR(0, 0, K)] + covC[COL_MAJOR(1, 1, K)] + covC[COL_MAJOR(2, 2, K)]);
                 a1 = ((covC[COL_MAJOR(1, 1, K)] * covC[COL_MAJOR(2, 2, K)]) - (covC[COL_MAJOR(1, 2, K)] * covC[COL_MAJOR(2, 1, K)]))
                     + ((covC[COL_MAJOR(0, 0, K)] * covC[COL_MAJOR(2, 2, K)]) - (covC[COL_MAJOR(0, 2, K)] * covC[COL_MAJOR(2, 0, K)]))
@@ -82,39 +104,34 @@ double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, Re
                 trLambda += sqrt(root1) + sqrt(root2) + sqrt(root3);
                 break;
             default:
+                // When K > 3, we use LAPACK to calculate the eigenvalues.
                 compute_k_eigenvalues(eigen, K);
                 for (int i = 0; i < K; i++)
                     trLambda += sqrt(eigen -> W[i]);
-
                 break;
         }
-
     } else {
 
+        // When we transform the coordinates, we need both the eigenvalues and eigenvectors.
+        //  For all K, we rely on LAPACK to calculate eigenpairs. We could do this manually 
+        //  for K = 1, 2, and 3, but the code would be somewhat lengthy.
         compute_k_eigenpairs(eigen, K);
 
+        // Calculate trLambda.
         for (int i = 0; i < K; i++)
             trLambda += sqrt(eigen -> W[i]);
 
+        // For convenience, we rename A and Z to U and V, respectively. 
         double* U = eigen -> A;
         double* V = eigen -> Z;
-        double* shift = eigen -> W;
 
+        // Caclulate our scaling factor rho.
         double rho = trLambda / trX;
 
-        /*
-        printf("\nEigenvectors:\n");
-        for (int i = 0; i < K; i++) {
-            for (int j = 0; j < K; j++)
-                printf("%lf\t", V[COL_MAJOR(K - i - 1, j, K)]);
-            printf("\n");
-        }
-        printf("\nEigenvalues:\n");
-        for (int i = 0; i < K; i++)
-            printf("%lf\t", sqrt(eigen -> W[K - i - 1]));
-        printf("\n");
-        */
-
+        // Calculate U using the equation Ui = (1 / sigma_i) * C * Vi
+        //  We could create another auxilary k-by-k array to store C, which would not require much memory
+        //  just more book-keeping. However, since k is usually small, we can recalculate C quickly.
+        //  We use eigen -> WORK[j] as a temporary vector.
         for (int i = 0; i < K; i++) {
             for (int j = 0; j < K; j++) {
                 eigen -> WORK[j] = 0;
@@ -129,61 +146,70 @@ double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, Re
             }
         }
 
-        /*
-        printf("\nU:\n");
+        // Define our shift vector. Known as b in the paper.
+        double* shift = eigen -> W;
+
+        printf("U = \n");
         for (int i = 0; i < K; i++) {
-            for (int j = 0; j < K; j++)
+            for (int j = 0; j < K; j++) {
                 printf("%lf\t", U[COL_MAJOR(i, j, K)]);
+            }
             printf("\n");
         }
-        */
+        printf("V = \n");
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < K; j++) {
+                printf("%lf\t", V[COL_MAJOR(i, j, K)]);
+            }
+            printf("\n");
+        }
 
+        // We calculate (A^T) = U(V^T). Again, eigen -> WORK is used as an auxilary vector.
+        //  NOTE: U is replaced by (A^T).
         for (int i = 0; i < K; i++) {
             for (int j = 0; j < K; j++) {
                 eigen -> WORK[j] = 0;
                 for (int k = 0; k < K; k++)
-                    eigen -> WORK[j] += U[COL_MAJOR(i, k, K)] * V[COL_MAJOR(K - j - 1, k, K)];
+                    eigen -> WORK[j] += U[COL_MAJOR(i, k, K)] * V[COL_MAJOR(K - k - 1, j, K)];
             }
-            shift[i] = 0;
+            // As we calculate, we can also calculate our shift vector.
             for (int j = 0; j < K; j++) {
                 U[COL_MAJOR(i, j, K)] = eigen -> WORK[j];
-                if (x0 != NULL && y0 != NULL)
-                    shift[i] += U[COL_MAJOR(i, j, K)] * x0[j];
+                if (x0 == NULL && y0 == NULL)
+                    shift[i] = 0;
+                else if (x0 == NULL)
+                    shift[i] = y0[i];
+                else if (y0 == NULL)
+                    shift[i] = -rho * U[COL_MAJOR(i, j, K)] * x0[i];
+                else
+                    shift[i] = y0[i] - rho * U[COL_MAJOR(i, j, K)] * x0[i];
             }
-            if (x0 != NULL && y0 != NULL)
-                shift[i] = y0[i] - rho * shift[i];
         }
 
-        /*
-        printf("\nA^T:\n");
+        printf("A^T = \n");
         for (int i = 0; i < K; i++) {
-            for (int j = 0; j < K; j++)
+            for (int j = 0; j < K; j++) {
                 printf("%lf\t", U[COL_MAJOR(i, j, K)]);
+            }
             printf("\n");
         }
-
-        printf("\nrho: %lf\n", rho);
-        printf("\ntrX: %lf\n", trX);
-        printf("\ntrY: %lf\n", trY);
-        printf("\ntrLambda: %lf\n", trLambda);
-
-        printf("\nShift:\n");
+        printf("rho = %lf\n", rho);
+        printf("b = \n");
         for (int i = 0; i < K; i++)
             printf("%lf\t", shift[i]);
-        printf("\n\n");
-        */
+        printf("\n");
 
+        // Transform each of the samples by the equation rho * (A^T) * X + shift.
         for (int i = 0; i < N; i++) {
+            // Sample vector times (A^T).
             for (int j = 0; j < K; j++) {
                 eigen -> WORK[j] = 0;
                 for (int k = 0; k < K; k++)
                     eigen -> WORK[j] += U[COL_MAJOR(j, k, K)] * Xc[i][k];
             }
-            for (int j = 0; j < K; j++) {
-                Xc[i][j] = rho * eigen -> WORK[j];
-                if (x0 != NULL && y0 != NULL)
-                    Xc[i][j] += shift[j];
-            }
+            // Scale and shift sample.
+            for (int j = 0; j < K; j++)
+                Xc[i][j] = rho * eigen -> WORK[j] + shift[j];
         }
 
     }
@@ -192,9 +218,11 @@ double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, Re
 
     return similarity ? sqrt(1 - statistic) : sqrt(statistic);
 
+    return 0;
+
 }
 
-double permutation_test(double** Xc, double** Yc, double** shuffleX, RealSymEigen* eigen, int n, int k, bool similarity, double t0, int NUM_PERMS) {
+double permutation_test(double** Xc, double** Yc, double** shuffleX, RealSymEigen_t* eigen, int n, int k, bool similarity, double t0, int NUM_PERMS) {
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < k; j++)
@@ -211,5 +239,33 @@ double permutation_test(double** Xc, double** Yc, double** shuffleX, RealSymEige
     }
 
     return ((double) numSig) / (NUM_PERMS + 1);
+
+}
+
+int main() {
+    int N = 4, K = 2;
+    double** X = (double**) calloc(N, sizeof(double*));
+    double* x0 = (double*) calloc(3, sizeof(double));
+    double** Y = (double**) calloc(N, sizeof(double*));
+    double* y0 = (double*) calloc(3, sizeof(double));
+    for (int i = 0; i < N; i++) {
+        X[i] = (double*) calloc(3, sizeof(double));
+        Y[i] = (double*) calloc(3, sizeof(double));
+    }
+    X[0][0] = -1; X[0][1] = 1; X[0][2] = 2;
+    X[1][0] =  1; X[1][1] = 1; X[1][2] = 3;
+    X[2][0] = -1; X[2][1] = -1; X[2][2] = -4;
+    X[3][0] =  1; X[3][1] = -1; X[3][2] = -5;
+    x0[0] = 1; x0[1] = 2; x0[2] = 3;
+
+    Y[0][0] = -1; Y[0][1] = 0; Y[0][2] = -1;
+    Y[1][0] =  0; Y[1][1] = 2; Y[1][2] = -3;
+    Y[2][0] = -2; Y[2][1] = 0; Y[2][2] = 5;
+    Y[3][0] =  1; Y[3][1] = 0; Y[3][2] = 7;
+    y0[0] = 1; y0[1] = -1; y0[2] = 5;
+
+    RealSymEigen_t* eigen = init_real_sym_eigen(K);
+
+    double t0 = procrustes_statistic(X, x0, Y, y0, eigen, N, K, true, false);
 
 }
--- src/ProcrustesAnalysis.h
+++ src/ProcrustesAnalysis.h
@@ -1,13 +1,49 @@
 
+// File: ProcrustesAnalysis.h
+// Date: 5 May 2024
+// Author: T. Quinn Smith
+// Principal Investigator: Dr. Zachary A. Szpiech
+// Purpose: Compute Procrustes statistic between two sets of points and perfrom permutation test.
+// References: Wang et al, Procrustes Analysis in Population Genetics, 2010.
+
 #ifndef _PROCRUSTES_ANALYSIS_H_
 #define _PROCRUSTES_ANALYSIS_H_
 
 #include "RealSymEigen.h"
-
 #include <stdbool.h>
 
-double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, RealSymEigen* eigen, int n, int k, bool transform, bool similarity);
+// Compute Procrustes statistic between two sets of points.
+// Accepts:
+//  double** Xc -> The n-by-k mean-centered query point set.
+//  double* x0 -> The k-dimensional vector holding the mean of each dimension.
+//                  If NULL, x0 is assumed to be the 0 vector.
+//  double** Yc -> The n-by-k mean centered target point set.
+//  double* y0 -> The k-dimensional vector holding the mean of each dimension.
+//                  If NULL, x0 is assumed to be the 0 vector.
+//  RealSymEigen_t* eigen -> Used to find eigen-pairs of k-by-k matrix. Allocated
+//                  memory is used to store covariance matrix even if Xc is not to be
+//                  transformed.
+//  int N -> The number of points.
+//  int K -> The dimension of each point.
+//  bool transform -> If set, x0 will be transformed by Procrustes analysis.
+//  bool similarity -> If set, statistic represents similarity between points. Otherwise, dissimilarity.
+// Returns: double, The Procrustes statistic.
+double procrustes_statistic(double** Xc, double* x0, double** Yc, double* y0, RealSymEigen_t* eigen, int N, int K, bool transform, bool similarity);
 
-double permutation_test(double** Xc, double** Yc, double** shuffleX, RealSymEigen* eigen, int n, int k, bool similarity, double t0, int NUM_PERMS);
+// Perfrom permutation test to evaluate the significance of the Procrustes statistic t0.
+// Accepts:
+//  double** Xc -> The n-by-k mean-centered query point set.
+//  double** Yc -> The n-by-k mean centered target point set.
+//  double** shuffleX -> A copy of Xc used to hold permuted rows of Xc.
+//  RealSymEigen_t* eigen -> Used to find eigen-pairs of k-by-k matrix. Allocated
+//                  memory is used to store covariance matrix. Points are not transformed 
+//                  during permutation test.
+//  int N -> The number of points.
+//  int K -> The dimension of each point.
+//  bool similarity -> If set, statistic represents similarity between points. Otherwise, dissimilarity.
+//  double t0 -> The Procrustes statistic used in the permutation test.
+//  int NUM_PERMS -> The number of permutations to execute.
+// Returns: double, The p-value obtained from the permutation test.
+double permutation_test(double** Xc, double** Yc, double** shuffleX, RealSymEigen_t* eigen, int N, int K, bool similarity, double t0, int NUM_PERMS);
 
 #endif
