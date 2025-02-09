0967041847c32cf2b42415e01bd970b7250e5dfb
TQ-Smith <thomasqsmith2000@gmail.com>
2025-02-08 20:50:47 -0500

    Better plots ...

--- plotPeaks.R
+++ plotPeaks.R
@@ -31,19 +31,25 @@ main <- function() {
                 SNP <- ".",
                 CHR <- as.numeric(factor(df$Chr, levels = unique(df$Chr))),
                 BP <- df["Start"],
-                P <- if (useT) lapply(df["t.stat"], function (x) 1 - x) else df["p.val"]
+                P <- df["p.val"]
             )
-            if (!useT) {
-                manhat <- manhat[!manhat$p.val == 0,]
+            if (useT) {
+                # Remove undefined t-stat windows. Take difference to observe magnitude.
+                manhat$P <- df["t.stat"]
+                manhat <- manhat[!manhat$P != -1,]
+                manhat$P <- lapply(manhat$P, function (x) 1 - x)
+            } else {
+                # Remove p values of 0.
+                manhat <- manhat[!manhat$P == 0,]
             }
             colnames(manhat) <- c("SNP", "CHR", "BP", "P")
             # Save plot to png.
             if (useT) {
                 png(paste(substr(fileName, 1, nchar(fileName) - 4), "_t.png", sep = ""), width = 1600, height = 1200, res = 300)
-                m <- manhattan(manhat, chr = "CHR", bp = "BP", p = "P", snp = "SNP", ylab = "-log(1-t)", main = substr(fileName, 1, length(fileName) - 4))
+                m <- manhattan(manhat, chr = "CHR", bp = "BP", p = "P", snp = "SNP", ylab = "-log(1-t)", main = substr(fileName, 1, length(fileName) - 4), xlim = c(min(manhat$BP - 1), max(manhat$BP + 1)), ylim = c(0, max(manhat$p) + 1))
             } else {
                 png(paste(substr(fileName, 1, nchar(fileName) - 4), "_p.png", sep = ""), width = 1600, height = 1200, res = 300)
-                m <- manhattan(manhat, chr = "CHR", bp = "BP", p = "P", snp = "SNP", main = substr(fileName, 1, length(fileName) - 4))
+                m <- manhattan(manhat, chr = "CHR", bp = "BP", p = "P", snp = "SNP", main = substr(fileName, 1, length(fileName) - 4), xlim = c(min(manhat$BP - 1), max(manhat$BP + 1)), ylim = c(0, max(manhat$p) + 1))
             }
             dev.off()
         }
